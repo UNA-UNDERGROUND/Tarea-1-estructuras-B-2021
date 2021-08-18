234814c338e485ccff365a79d64eccae2ab12ad8
Jonathan Guzmán <jonguz5@hotmail.com>
2021-08-17 18:59:29 -0600

    programa 2 hecho

--- programa2/src/source.cpp
+++ programa2/src/source.cpp
@@ -1,3 +1,18 @@
-int main(){
-    
+#include <iostream>
+#include <string>
+#include <utility> // std::swap
+
+int main() {
+	using std::string;
+
+	string *a = new string("soy el string A");
+	string *b = new string("soy el string B");
+
+	std::cout << "a[0x" << a << "] = " << *a << "\n";
+	std::cout << "b[0x" << b << "] = " << *b << "\n";
+
+	std::swap(a, b);
+
+	std::cout << "a[0x" << a << "] = " << *a << "\n";
+	std::cout << "b[0x" << b << "] = " << *b << "\n";
 }
